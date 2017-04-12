735042460547b5325f8f9c793e0e101357df23b4
Andriy Volosh <andriy.volosh@gmail.com>
2017-04-12 15:39:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,28 @@
-<?php
-
-$count = 15;
-$arr = array();
-for ($i = 0; $i < $count; $i++) {
-    $arr[$i] = rand();
-}
-echo '<b>Array with 15 elements:</b>' . '<br/>';
-var_dump($arr);
-echo '<br/>';
-$min = min($arr);
-$max = max($arr);
-echo '<b>Minimal element: </b>' . $min . '<br/><b>Maximal element: </b>' . $max . '<br/>';
-$reversed = array_reverse($arr);
-echo '<b>Reversed array:</b>' . '<br/>';
-var_dump($reversed);
-echo '<br/>';
-$sorted = asort($arr);
-echo '<b>Sorted array:</b>' . '<br/>';
-var_dump($arr);
-echo '<br/>';
-$sum = array_sum($arr);
-echo '<b>Summ of elements:</b> ' . $sum . '<br/>';
-$key = array_search(33, $arr);
-if ($key === FALSE) {
-    echo 'This array don\'t have number 33.' . '<br/>';
-} else {
-    echo 'This array have number 33.' . '<br/>';
-}
-$shuffle = shuffle($arr);
-echo '<b>Shuffled array:</b>' . '<br/>';
-var_dump($arr);
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link href="css/style1.css" rel="stylesheet" type="text/css"/>
+    <title>Создать шаблон страницы</title>
+</head>
+<body>
+    <form name="create_template" action="create_template.php" method="GET">
+        <label>Укажите цвет фона:
+            <input type="color" name="background"/>
+        </label>
+        <label>Текст заголовка веб-страницы:
+            <input type="text" name="title" value="RedTie"/>
+        </label>
+        <label>Абсолютный путь к картинке логотипа:
+            <input type="url" name="url" value="http://static.oswd.org/designs/3699/RedTie/images/tie_logo.gif"/>
+        </label>
+        <label>Название сайта:
+            <input type="text" name="name_logo" value="RedOTieO"/>
+        </label>
+        <label>Текст футера:
+            <input type="text" name="footer" value="Your Company Name &copy; 2017"/>
+        </label>
+        <input type="submit" value="Отправить"/>
+    </form>
+</body>
+</html>
