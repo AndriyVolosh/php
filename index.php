c1f9bbcb5b6413a4d43424cf6512e49b8ec4d3fc
Andriy Volosh <andriy.volosh@gmail.com>
2017-05-11 10:25:57 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,36 @@
 <?php
-
-$count = 15;
-$arr = array();
-for ($i = 0; $i < $count; $i++) {
-    $arr[$i] = rand();
+$photos = array_slice(scandir('images/gallery/'), 2);
+foreach ($photos as $file_name) {
+    $output .= '<img height="100px" src="' . 'images/gallery/' . $file_name . '">';
 }
-echo '<b>Array with 15 elements:</b>' . '<br/>';
-var_dump($arr);
-echo '<br/>';
-$min = min($arr);
-$max = max($arr);
-echo '<b>Minimal element: </b>' . $min . '<br/><b>Maximal element: </b>' . $max . '<br/>';
-$reversed = array_reverse($arr);
-echo '<b>Reversed array:</b>' . '<br/>';
-var_dump($reversed);
-echo '<br/>';
-$sorted = asort($arr);
-echo '<b>Sorted array:</b>' . '<br/>';
-var_dump($arr);
-echo '<br/>';
-$sum = array_sum($arr);
-echo '<b>Summ of elements:</b> ' . $sum . '<br/>';
-$key = array_search(33, $arr);
-if ($key === FALSE) {
-    echo 'This array don\'t have number 33.' . '<br/>';
-} else {
-    echo 'This array have number 33.' . '<br/>';
-}
-$shuffle = shuffle($arr);
-echo '<b>Shuffled array:</b>' . '<br/>';
-var_dump($arr);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Gallery</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-4 col-xs-6">
+                    <img class="img-responsive logo" src="img/gnlv6j7z.bmp"/>
+                </div>
+                <div class="col-md-9 col-sm-6 col-xs-6">
+                    <h1 class="namesite">Bootstrap Gallery</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <?= $output ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-sm-6 col-xs-6">
+                    <p class="footer"> &copy; 2017 Bootstrap Gallery</p>
+                </div>
+        </div>
+    </body>
+</html>
