88228b9054f2fa45d8caedfc6ba114a403ee782a
Andriy Volosh <andriy.volosh@gmail.com>
2017-05-17 08:19:20 +0300

    Update reg.php

--- reg.php
+++ reg.php
@@ -1,3 +1,14 @@
+<?php
+if ($_POST) {
+    $file = 'files/users.db';
+    $user=array();
+    $user['login']=$_POST['login'];
+    $user['password']=$_POST['password'];
+    var_dump($user);
+    $string = serialize($user);
+    $f = file_put_contents($file, $string, FILE_APPEND);
+}
+?>
 
 
 <!DOCTYPE html>
@@ -7,17 +18,17 @@
         <title></title>
     </head>
     <body>
-	<form action="#" method="POST">
-	    <label>Login: 
-	    <input type="text" name="login" required/>
-	    </label><br><br>
-	    <label>Password: 
-	    <input type="text" name="password" required/>
-	    </label><br><br>
-	    <label>Confirm password: 
-	    <input type="text" name="password" required/>
-	    </label><br><br>
-	    <input type="submit" name="reg" value="Зарегистрироваться"/>
-	</form>
+        <form action="#" method="POST">
+            <label>Login: 
+                <input type="text" name="login" required/>
+            </label><br><br>
+            <label>Password: 
+                <input type="text" name="password" required/>
+            </label><br><br>
+            <label>Confirm password: 
+                <input type="text" name="password" required/>
+            </label><br><br>
+            <input type="submit" name="reg" value="Зарегистрироваться"/>
+        </form>
     </body>
 </html>
