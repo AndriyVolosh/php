eaebf1975ee034a3865c2c71ba5a92355621f094
Andriy Volosh <andriy.volosh@gmail.com>
2017-05-17 08:18:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,23 @@
 <?php
+include_once 'includes/config.php';
+$users = getUsers();
+if (!is_array($users)) {
+    $users = array();
+}
+?>
 
-$count = 15;
-$arr = array();
-for ($i = 0; $i < $count; $i++) {
-    $arr[$i] = rand();
-}
-echo '<b>Array with 15 elements:</b>' . '<br/>';
-var_dump($arr);
-echo '<br/>';
-$min = min($arr);
-$max = max($arr);
-echo '<b>Minimal element: </b>' . $min . '<br/><b>Maximal element: </b>' . $max . '<br/>';
-$reversed = array_reverse($arr);
-echo '<b>Reversed array:</b>' . '<br/>';
-var_dump($reversed);
-echo '<br/>';
-$sorted = asort($arr);
-echo '<b>Sorted array:</b>' . '<br/>';
-var_dump($arr);
-echo '<br/>';
-$sum = array_sum($arr);
-echo '<b>Summ of elements:</b> ' . $sum . '<br/>';
-$key = array_search(33, $arr);
-if ($key === FALSE) {
-    echo 'This array don\'t have number 33.' . '<br/>';
-} else {
-    echo 'This array have number 33.' . '<br/>';
-}
-$shuffle = shuffle($arr);
-echo '<b>Shuffled array:</b>' . '<br/>';
-var_dump($arr);
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+	<ul>
+	    <?php foreach ($users as $key => $user): ?>
+		<li><?= $key.' => '.$user ?></li>
+	    <?php endforeach; ?>
+	</ul>
+	<a href="reg.php">Регистрация</a>
+    </body>
+</html>
